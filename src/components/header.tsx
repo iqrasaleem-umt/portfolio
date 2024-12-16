6b1137d5165c973859e7d64a53c3d4abef6dbcb0
iqra <fhsaleem8104@gmail.com>
2024-12-16 12:19:43 +0500

    update

---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <div className="bg-green-800 w-full h-8 flex items-center justify-between px-4 ">
+      {/* Left Section: IQRASALEEM MADE */}
+      <div className="flex items-center space-x-2">
+        <p className="text-white">IQRASALEEM</p>
+        <p className="text-green-300">MADE</p>
+      </div>
+
+      {/* Center Section: Portfolio with Arrows */}
+      <div className="flex items-center space-x-2">
+        {/* Backward Arrow */}
+        <Image
+          src="/arrowback.png"
+          height={15}
+          width={15}
+          alt="backward arrow"
+          className="filter invert"
+        />
+        {/* Portfolio Text */}
+        <p className="text-white text-center">Portfolio</p>
+        {/* Forward Arrow */}
+        <Image
+          src="/arrowfor.png"
+          height={15}
+          width={15}
+          alt="forward arrow"
+          className="filter invert"
+        />
+      </div>
+    </div>
+  )
+}
---
+++ src/components/hero.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <div>
+         <div className="relative w-full h-[550px]">
+        {/* Background Image */}
+        <Image
+          className="object-cover"
+          src="/portfoliogirl.jpg"
+          alt="Farming Background"
+          layout="fill"
+          objectFit="cover"
+        />
+       </div>
+       <div className="absolute inset-0 flex flex-col items-center justify-center px-6  transform transition-transform duration-500 hover:scale-105 mt-48">
+          <h1 className="text-white text-2xl md:text-4xl font-bold">
+          IQRA SALEEM
+          </h1>
+          <p className="text-white font-bold text-2xl"> I am Frontend Developer</p>
+          </div>
+    </div>
+    
+  )
+}
